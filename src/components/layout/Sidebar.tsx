c0c4bf504e4fa0ec736826963903c0093036c718
Bas Veltenaar <basveltenaar@gmail.com>
2026-07-16 15:35:18 +0200

    udpate

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -169,13 +169,13 @@ const PRAKTIJKPLANNER_MANAGER_NAV_ITEMS: NavItem[] = [
   },
   {
     id: 'praktijkplanner-capaciteitsplanner',
-    label: 'Capaciteit planner',
+    label: 'Capaciteiten planner',
     href: '/praktijkplanner/capaciteitsplanner',
     icon: <MapPin className="size-4 shrink-0" />,
   },
   {
     id: 'praktijkplanner-capaciteitsoverzicht',
-    label: 'Capaciteit overzicht',
+    label: 'Capaciteiten overzicht',
     href: '/praktijkplanner/capaciteitsoverzicht',
     icon: <Check className="size-4 shrink-0" />,
   },
